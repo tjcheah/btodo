cb55ff7804458c60804f8bd08d43feae9e7fe2a5
Tatiana Cheah <tatiana.cheah@gmail.com>
2021-10-20 10:07:20 +0800

    add recommended

--- src/App.js
+++ src/App.js
@@ -3,9 +3,27 @@ import data_array from "./obj.js";
 
 function App() {
   var filtered = data_array.filter((data) => data.type === "charity");
-  const mapping = (
-    <div>
+  var recommends = data_array.filter(
+    (data) =>
+      data.price === 0.0 && data.accessibility === "Few to no challenges"
+  );
+  const mappingFil = (
+    <div className="category">
       {filtered.map((list) => {
+        return (
+          <div id="card" key={list.id}>
+            <p>Activity: {list.activity}</p>
+            <p>No. of participants: {list.participants}</p>
+            <p>Type: {list.type}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+
+  const mappingRecs = (
+    <div className="recommended">
+      {recommends.slice(0, 5).map((list) => {
         return (
           <div id="card" key={list.id}>
             <p>Activity: {list.activity}</p>
@@ -19,7 +37,10 @@ function App() {
   return (
     <div id="bored-container">
       <h1>Bored?</h1>
-      {mapping}
+      <h2>Recommended</h2>
+      {mappingRecs}
+      <h2>Category: Charity</h2>
+      {mappingFil}
     </div>
   );
 }
